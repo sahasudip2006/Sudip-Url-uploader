9febc041c623b680d06f7c8203353bbc31a606df
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-01-10 08:16:08 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -20,3 +20,27 @@ async def button(bot, update):
         await youtube_dl_call_back(bot, update)
     elif "=" in cb_data:
         await ddl_call_back(bot, update)
+
+
+@Clinton.on_callback_query()
+async def button(bot, update):
+    if update.data == "home":
+        await update.message.edit(
+            text=Translation.START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.START_BUTTONS,
+            # disable_web_page_preview=True
+        )
+    elif update.data == "help":
+        await update.message.edit(
+            text=Translation.HELP_TEXT,
+            reply_markup=Translation.HELP_BUTTONS,
+            # disable_web_page_preview=True
+        )
+    elif update.data == "about":
+        await update.message.edit(
+            text=Translation.ABOUT_TEXT,
+            reply_markup=Translation.ABOUT_BUTTONS,
+            # disable_web_page_preview=True
+        )
+    elif "close" in update.data:
+        await update.message.delete(True)
