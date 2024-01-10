18fedf6f17e010b49c688aaa0a373548f39501c1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-01-10 08:25:04 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -6,6 +6,7 @@ from pyrogram import filters
 from pyrogram import Client as Clinton
 from plugins.youtube_dl_button import youtube_dl_call_back
 from plugins.dl_button import ddl_call_back
+from translation import Translation
 
 @Clinton.on_callback_query(filters.regex('^X0$'))
 async def delt(bot, update):
